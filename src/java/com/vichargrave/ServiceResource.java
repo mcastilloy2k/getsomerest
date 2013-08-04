994fdaca1e2d7734375ab32a971538da580b9d01
vichargrave <vichargrave@gmail.com>
2013-08-04 16:45:11 -0700

    Changed name of main service code file in the comments section.

--- src/java/com/vichargrave/ServiceResource.java
+++ src/java/com/vichargrave/ServiceResource.java
@@ -1,11 +1,11 @@
 /*
-   GetSomeRest.java
+   ServiceResource.java
 
    Sample RESTful Java Web Service 
 
    ------------------------------------------
 
-   Copyright © 2013 [Vic Hargrave - http://vichargrave.com]
+   Copyright ï¿½ 2013 [Vic Hargrave - http://vichargrave.com]
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
